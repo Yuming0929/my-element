dbbf162c514c458806f7285998615f86a5d5ffbd
Yuming Huang <yumingh0929@gmail.com>
2025-08-23 23:39:27 -0700

    :tdata first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * 
+ * Button.vue
+ * Button.test.tsx
+ * types.ts
+ * style.css
+ * constants.ts
+ */
+
+ defineOptions({
+    name: 'ErButton'
+ })
+
+
+</script>
+
+<template>
+    <button style="color: red">this is a button</button>
+</template>
